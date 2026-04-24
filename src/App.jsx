199aef5f2f24d70fdc000309990737ab9cab8758
Kemzhub <gb93518@gmail.com>
2026-04-24 14:44:06 +0700

    feat : mengubah tanda squeres() menjadi squeres[]

--- src/App.jsx
+++ src/App.jsx
@@ -20,15 +20,15 @@ export default function Board() {
 
   return (
     <div className="board">
-      <Squere value={squeres(0)} onSquereClick={() => handleClick(0)} />
-      <Squere value={squeres(1)} onSquereClick={() => handleClick(1)} />
-      <Squere value={squeres(2)} onSquereClick={() => handleClick(2)} />
-      <Squere value={squeres(3)} onSquereClick={() => handleClick(3)} />
-      <Squere value={squeres(4)} onSquereClick={() => handleClick(4)} />
-      <Squere value={squeres(5)} onSquereClick={() => handleClick(5)} />
-      <Squere value={squeres(6)} onSquereClick={() => handleClick(6)} />
-      <Squere value={squeres(7)} onSquereClick={() => handleClick(7)} />
-      <Squere value={squeres(8)} onSquereClick={() => handleClick(8)} />
+      <Squere value={squeres[0]} onSquereClick={() => handleClick(0)} />
+      <Squere value={squeres[1]} onSquereClick={() => handleClick(1)} />
+      <Squere value={squeres[2]} onSquereClick={() => handleClick(2)} />
+      <Squere value={squeres[3]} onSquereClick={() => handleClick(3)} />
+      <Squere value={squeres[4]} onSquereClick={() => handleClick(4)} />
+      <Squere value={squeres[5]} onSquereClick={() => handleClick(5)} />
+      <Squere value={squeres[6]} onSquereClick={() => handleClick(6)} />
+      <Squere value={squeres[7]} onSquereClick={() => handleClick(7)} />
+      <Squere value={squeres[8]} onSquereClick={() => handleClick(8)} />
     </div>
   );
 }
